ac29883d86963e0c6822957898793a3396e814d4
Omutnykh Oleg <omutnih1234@gmail.com>
2022-09-12 09:58:25 +0500

    Создан репозиторий с лабораторными работами по C#

---
+++ basa/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace basa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+
+
+
+
+/* int n;
+ * bool isConvect = Int.TryParse(buf, out n);
+ * if (isConvert) { ... }
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+/* Тестирование
+ * 1 страница: Тест(фото)
+ * 2 страница: Чёрный ящик(фото)
+ * 3 страница: Белый ящик(фото)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
