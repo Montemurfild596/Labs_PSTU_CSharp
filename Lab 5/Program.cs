ef22003bf880504c5834f05ce680bc59b3e3187a
Omutnykh Oleg <omutnih1234@gmail.com>
2022-10-18 12:26:19 +0500

    5 лабораторная: добавлены начальные функции заполнения массивов и функция ввода границ рандомного заполнения

--- Lab 5/Program.cs
+++ Lab 5/Program.cs
@@ -5,6 +5,81 @@ namespace Lab_5
 {
 	class Program
 	{
+		static void InputRandomBoundaries(out int a, out int b)
+        {
+			string buf;
+
+			// переменные для проверки границ универсума (верхняя больше нижней, обе положительные и целочисленные)
+			bool isCorrectLeftBoundary, isCorrectRightBoundary, isUpperMoreThanLower = false, isNotAllRight = false;
+			do
+			{
+				Console.Write("Введите нижнюю границу универсума: ");
+				buf = Console.ReadLine();
+				isCorrectLeftBoundary = Int32.TryParse(buf, out a);
+				Console.Write("Введите верхнюю границу универсума: ");
+				buf = Console.ReadLine();
+				isCorrectRightBoundary = Int32.TryParse(buf, out b);
+				if (!isCorrectRightBoundary || !isCorrectLeftBoundary)
+				{
+					if (!isCorrectRightBoundary && !isCorrectLeftBoundary)
+					{
+						Console.WriteLine("Ошибка: значения нижней и верхней границ универсума не соответствует типу integer");
+					}
+					else if (!isCorrectRightBoundary)
+					{
+						Console.WriteLine("Ошибка: значение верхней границы универсума не соответствует типу integer");
+					}
+					else
+					{
+						Console.WriteLine("Ошибка: значение нижней границы универсума не соответствует типу integer");
+					}
+				}
+				else
+				{
+					isUpperMoreThanLower = b >= a;
+					if (!isUpperMoreThanLower)
+					{
+						Console.WriteLine("Ошибка: значение нижней границы универсума больше верхней границы");
+					}
+				}
+				isNotAllRight = !isCorrectRightBoundary || !isCorrectLeftBoundary || !isUpperMoreThanLower;
+			} while (isNotAllRight);
+		}
+		// функция заполнения одномерного массива случайными числами
+		static void RandomFillingArray(int []arr)
+        {
+			int lower, upper;
+
+			for (int i = 0; i < arr.Length; ++i)
+            {
+
+            }
+        }
+		// функция заполнения двумерного массива случайными числами
+		static void RandomFillingArray(int [,]arr)
+        {
+
+        }
+		// функция заполнения рваного массива случайными числами
+		static void RandomFillingArray(int[][] arr)
+		{
+
+		}
+		// функция заполнения одномерного массива значениями пользователя
+		static void userFillingArray(int []arr)
+        {
+
+        }
+		// функция заполнения двумерного массива значениями пользователя
+		static void userFillingArray(int [,]arr)
+        {
+
+        }
+		// функция заполнения рваного массива значениями пользователя
+		static void userFillingArray(int[][] arr)
+		{
+
+		}
 		static void Main(string[] args)
 		{
 			Random rnd = new Random();
