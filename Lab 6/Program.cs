3711cebc26340b93f88561146abf898ef4c98bbd
Omutnykh Oleg <omutnih1234@gmail.com>
2022-11-16 22:29:19 +0500

    6 лабораторная: Добавлена функция ввода границ для ДСЧ

--- Lab 6/Program.cs
+++ Lab 6/Program.cs
@@ -1,10 +1,53 @@
 ﻿using System;
+using MyClassLibrary;
 
 namespace Lab_6
 {
     class Program
     {
-        static void Main(string[] args)
+		static void InputBoundaries(out int a, out int b, string message)
+		{
+			string buf;
+			bool isCorrectLeftBoundary, isCorrectRightBoundary, isUpperMoreThanLower = false, isNotAllRight;
+			do
+			{
+				Console.WriteLine(message);
+				Console.Write("Введите нижнюю границу: ");
+				buf = Console.ReadLine();
+				isCorrectLeftBoundary = Int32.TryParse(buf, out a);
+				Console.Write("Введите верхнюю границу: ");
+				buf = Console.ReadLine();
+				isCorrectRightBoundary = Int32.TryParse(buf, out b);
+				if (!isCorrectRightBoundary || !isCorrectLeftBoundary)
+				{
+					if (!isCorrectRightBoundary && !isCorrectLeftBoundary)
+					{
+						Console.WriteLine("Ошибка: значения нижней и верхней границ не соответствуют типу integer");
+					}
+					else if (!isCorrectRightBoundary)
+					{
+						Console.WriteLine("Ошибка: значение верхней границы не соответствует типу integer");
+					}
+					else
+					{
+						Console.WriteLine("Ошибка: значение нижней границы не соответствует типу integer");
+					}
+				}
+				else
+				{
+					isUpperMoreThanLower = b >= a;
+					if (!isUpperMoreThanLower)
+					{
+						Console.WriteLine("Ошибка: значение нижней границы больше верхней границы");
+					}
+				}
+				isNotAllRight = !isCorrectRightBoundary || !isCorrectLeftBoundary || !isUpperMoreThanLower;
+			} while (isNotAllRight);
+		}
+
+
+
+		static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
